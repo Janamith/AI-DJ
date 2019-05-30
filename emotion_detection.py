0eddf06eb794b62d22e99018883801e8ff4c5b6c
Jeff Setter <jos36@cornell.edu>
2019-05-30 15:40:03 -0700

    Add song files for emotions

--- emotion_detection.py
+++ emotion_detection.py
@@ -5,18 +5,24 @@ import time
 import numpy as np 
 from keras.models import load_model
 
+import os
+import time
+
 #vs = VideoStream(src=0).start()
 video_capture = cv2.VideoCapture(0)
 #vs = VideoStream(usePiCamera=True).start()
 time.sleep(1.0)
 
-target_emotions = ['anger', 'happiness']
+target_emotions = ['neutral', 'happiness', 'sad']
 model = load_model('./models/_mini_XCEPTION.102-0.66.hdf5')
 model.get_config()
 
+emotion_results = []
 target = ['angry', 'disgust', 'fear', 'happy', 'sad', 'surprise', 'neutral']
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 while True:
+	start_time = time.time()
+        
 	ret, frame = video_capture.read()
 	frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 	faces = face_cascade.detectMultiScale(frame_gray, scaleFactor=1.1)
@@ -29,8 +35,15 @@ while True:
 		face_crop = np.asarray(face_crop)
 		face_crop = face_crop.reshape(1,face_crop.shape[0], face_crop.shape[1],1)
 		result = target[np.argmax(model.predict(face_crop))]
+                
+                emotion_results.append(result)
+		print(result)
 		cv2.putText(frame, result, (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (200, 0, 0), 3, cv2.LINE_AA)
 
+	if (time.time() - start_time >= 10):
+                dominant_emotion = max(emotion_results, key=emotion_results.count)
+                print("dominant emotion:", dominant_emotion)
+                
 	cv2.imshow('frame', frame)
 	if cv2.waitKey(1) & 0xFF == ord('q'):
 		break
